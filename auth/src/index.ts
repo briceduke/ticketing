3f964dbb17d2d78a6e9a9c77c5196e2ced165860
Brice Duke <bricewduke@gmail.com>
2021-09-26 16:25:30 -0500

    chore(global): set up cloud and local build

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('hi');
+});
+
 app.listen(port, () => {
-	console.log(`AUTH >> Running on http://localhost:${port}`);
+	console.log(`Running on http://localhost:${port}`);
 });
